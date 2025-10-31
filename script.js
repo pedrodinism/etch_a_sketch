99a6777c663a046c4b18c05c14420872da6b26e6
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2025-10-31 22:14:42 +0100

    made the generateGrid action dinamic, to take into account the width and number of squares, added buttons instead of the input and added some css

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-function generateDivs (n, parent) {
+function generateGrid (n, parent) {
+    parent.innerHTML = ""
+    const size = 600 / Math.sqrt(n)
     for (let i = 0; i < n; i++) {
         let newDiv = document.createElement("div")
+        newDiv.style.height = size + "px"
+        newDiv.style.width = size + "px"
+        newDiv.addEventListener("mouseover", () => {
+            newDiv.style.background = 'black'
+        })
         parent.appendChild(newDiv)
     }
 }
 
-let inp = document.querySelector("#input_count")
 let grid = document.querySelector("#grid")
-let btn = document.querySelector("button")
+let btn = document.querySelector(".buttons")
 
-btn.addEventListener("click", () => {
-    generateDivs(inp.value, grid)
+btn.addEventListener("click", (event) => {
+    let target = event.target.id
+    switch (target) {
+        case "smallGrid":
+            generateGrid(100, grid)
+            return
+        case "mediumGrid":
+            generateGrid(400, grid)
+            return
+        case "bigGrid":
+            generateGrid(900, grid)
+            return
+    }    
 })
